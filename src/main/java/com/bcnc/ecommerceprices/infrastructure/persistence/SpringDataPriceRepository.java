3602db7199bcbbbd31248184290f41171000fb4e
mvallsz <mvallsz85@gmail.com>
2025-06-10 01:13:03 +0200

    fix: se ajusto el query y se eliminaron los valores no validos doc: se agrego documentacion (javaDoc)

--- src/main/java/com/bcnc/ecommerceprices/infrastructure/persistence/SpringDataPriceRepository.java
+++ src/main/java/com/bcnc/ecommerceprices/infrastructure/persistence/SpringDataPriceRepository.java
@@ -1,5 +1,6 @@
 package com.bcnc.ecommerceprices.infrastructure.persistence;
 
+import com.bcnc.ecommerceprices.infrastructure.persistence.entity.PriceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,11 +9,24 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 import java.util.List;
 
+/**
+ * Repository interface for accessing PriceEntity data in the database.
+ * Provides methods for querying price-related information.
+ */
 @Repository
 public interface SpringDataPriceRepository extends JpaRepository<PriceEntity, Long> {
 
-    @Query(value = "SELECT p FROM PriceEntity p WHERE p.productId = :productId AND p.brand.id = :brandId AND :applicationDate BETWEEN p.startDate AND p.endDate ORDER BY p.priority DESC LIMIT 1")
-    List<PriceEntity> findTopApplicablePriceEntity(
+    /**
+     * Finds the applicable price entity based on the application date, product ID, and brand ID.
+     * Order results by priority in descending order.
+     *
+     * @param applicationDate the date for which the price is applicable
+     * @param productId       the ID of the product
+     * @param brandId         the ID of the brand
+     * @return a list of PriceEntity objects matching the criteria, ordered by priority
+     */
+    @Query(value = "SELECT p FROM PriceEntity p WHERE p.productId = :productId AND p.brand.id = :brandId AND :applicationDate BETWEEN p.startDate AND p.endDate ORDER BY p.priority DESC")
+    List<PriceEntity> findApplicablePriceEntities(
             @Param("applicationDate") LocalDateTime applicationDate,
             @Param("productId") Long productId,
             @Param("brandId") Long brandId);

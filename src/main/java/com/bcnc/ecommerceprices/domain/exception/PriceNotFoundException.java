f50e2d657d973d18a62edfa97192f3d3a341c666
mvallsz <mvallsz85@gmail.com>
2025-06-10 01:11:17 +0200

    doc: se agrego documentacion (javaDoc)

--- src/main/java/com/bcnc/ecommerceprices/domain/exception/PriceNotFoundException.java
+++ src/main/java/com/bcnc/ecommerceprices/domain/exception/PriceNotFoundException.java
@@ -1,7 +1,15 @@
 package com.bcnc.ecommerceprices.domain.exception;
 
+/**
+ * Exception thrown when no applicable price is found for a given product, brand, and application date.
+ */
 public class PriceNotFoundException extends RuntimeException {
 
+    /**
+     * Constructs a new PriceNotFoundException with the specified detail message.
+     *
+     * @param message the detail message explaining the reason for the exception
+     */
     public PriceNotFoundException(String message) {
         super(message);
     }
